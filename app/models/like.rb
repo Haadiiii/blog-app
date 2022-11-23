6fd214d9e9076ed0b6a6ee9877d24bf6ffede861
Haadiiii <hammali99@gmail.com>
2022-11-23 22:24:37 +0500

    resolved error

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :author, foreign_key: :author_id
 
   def update_like_counter
-    author.increment!(:likes_counter)
+    post.increment!(:likes_counter)
   end
 end
