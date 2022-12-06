5e39e8d2b87e0708f25448274ff385be9982c366
niicode <niidarku66@gmail.com>
2022-12-06 12:43:29 +0000

    Add User show test

--- spec/feature/user_spec.rb
+++ spec/feature/user_spec.rb
@@ -32,4 +32,47 @@ RSpec.describe "UserShows", type: :system do
     end
   end
 
+  describe 'show page' do
+    it "I can see the user's profile picture." do
+      visit user_path(subject.id)
+      page.has_css?('.img-fluid')
+    end
+    it "I can see the user's username." do
+      visit user_path(subject.id)
+      page.has_content?(subject.name)
+    end
+    it 'I can see the number of posts the user has written.' do
+      visit user_path(subject.id)
+      page.has_content?(subject.posts_counter)
+    end
+    it "I can see the user's bio." do
+      visit user_path(subject.id)
+      page.has_content?(subject.bio)
+    end
+    it "I can see the user's first 3 posts." do
+      Post.create([{ author: subject, title: 'First Post', text: 'My first post' },
+                   { author: subject, title: 'Second Post', text: 'My Second post' }, { author: subject, title: 'Third Post', text: 'My Third post' }])
+      visit user_path(subject.id)
+      page.has_content?(subject.posts)
+    end
+    it "I can see a button that lets me view all of a user's posts." do
+      visit user_path(subject.id)
+      page.has_button?('See All Posts')
+    end
+    it "When I click a user's post, it redirects me to that post's show page." do
+      post = Post.create(author: subject, title: 'First Post', text: 'My first post')
+      visit user_path(subject.id)
+      click_on 'First Post'
+      visit user_post_path(subject.id, post.id)
+      page.has_content?(post.title)
+    end
+    it "When I click to see all posts, it redirects me to the user's post's index page." do
+      visit user_path(subject.id)
+      sleep(5)
+      click_on 'See All Posts'
+      visit user_posts_path(subject.id)
+      page.has_content?('John')
+    end
+  end
+
 end
